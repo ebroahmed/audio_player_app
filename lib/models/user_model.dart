26e65b1ffc10de903170cf56564f2138539b5f31
Ebrahim <ebrahimahmed804853@gmail.com>
2025-08-08 16:00:39 +0300

    user model created

---
+++ lib/models/user_model.dart
@@ -0,0 +1,34 @@
+class AppUser {
+  final String uid;
+  final String displayName;
+  final String email;
+  final String? profileImageUrl;
+  final DateTime createdAt;
+
+  AppUser({
+    required this.uid,
+    required this.displayName,
+    required this.email,
+    this.profileImageUrl,
+    required this.createdAt,
+  });
+
+  factory AppUser.fromMap(Map<String, dynamic> map, String uid) {
+    return AppUser(
+      uid: uid,
+      displayName: map['displayName'] ?? '',
+      email: map['email'] ?? '',
+      profileImageUrl: map['profileImageUrl'],
+      createdAt: (map['createdAt'] as DateTime?) ?? DateTime.now(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'displayName': displayName,
+      'email': email,
+      'profileImageUrl': profileImageUrl,
+      'createdAt': createdAt,
+    };
+  }
+}
